95459f500ee7c593d3511e8755e3e8fb9b03adef
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-08-03 16:11:02 -0300

    Desenvolvido o Service de Usuario

---
+++ src/main/java/com/cadastro/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cadastro.service.impl;
+
+import com.cadastro.service.UsuarioService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+}
