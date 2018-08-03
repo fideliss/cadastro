423bd94ba557612009c78f830fb5ac97e0a5fd8a
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-08-03 15:35:19 -0300

    Criação da entidade Usuario

---
+++ src/main/java/com/cadastro/entity/Usuario.java
@@ -0,0 +1,53 @@
+package com.cadastro.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Usuario implements Serializable {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "nome", nullable = false)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "senha", nullable = false)
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
