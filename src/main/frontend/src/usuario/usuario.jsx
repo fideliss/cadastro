126532cd987c2deb9c777fba31782e3e47967e9c
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-08-07 15:06:33 -0300

    Implementação do controle dos campos do formulário.

--- src/main/frontend/src/usuario/usuario.jsx
+++ src/main/frontend/src/usuario/usuario.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import UsuarioForm from './usuarioForm'
+
+const URL = 'http://localhost:8080/api/cadastro/'
 
 export default class Usuario extends Component {
     constructor(props) {
         super(props)
         this.state = { nome: '', email: '', senha: '', list:[] }
+
+        this.onChangeNome = this.onChangeNome.bind(this)
+        this.onChangeEmail = this.onChangeEmail.bind(this)
+        this.onChangeSenha = this.onChangeSenha.bind(this)
+        this.salvaUsuario = this.salvaUsuario.bind(this)
+    }
+
+    onChangeNome(e) {
+        this.setState({ ...this.state, nome: e.target.value })
+    }
+
+    onChangeEmail(e) {
+        this.setState({ ...this.state, email: e.target.value })
+    }
+
+    onChangeSenha(e) {
+        this.setState({ ...this.state, senha: e.target.value })
+    }
+
+    salvaUsuario() {
+        console.log(this.state.nome + this.state.email + this.state.senha)
     }
 
     render() {
         return (
             <div>
                 <PageHeader />
-                <UsuarioForm />
+                <UsuarioForm 
+                    nome={this.state.nome} 
+                    email={this.state.email} 
+                    senha={this.state.senha} 
+                    onChangeNome={this.onChangeNome} 
+                    onChangeEmail={this.onChangeEmail} 
+                    onChangeSenha={this.onChangeSenha} 
+                    onClickSalvar={this.salvaUsuario} />
             </div>
         )
     }
--- src/main/frontend/src/usuario/usuarioForm.jsx
+++ src/main/frontend/src/usuario/usuarioForm.jsx
@@ -1,37 +1,22 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React from 'react'
 
-class UsuarioForm extends Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
-        return (
-            <form>
-                <div className="form-group">
-                    <label>Nome</label>
-                    <input type="text" className="form-control" id="nome" placeholder="Nome" value={this.props.nome} />
-                </div>
-                <div className="form-group">
-                    <label>E-mail</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                    <label>Senha</label>
-                    <input type="senha" className="form-control" id="senha" placeholder="senha" />
-                </div>
-                <button type="submit" className="btn btn-primary">Salvar</button>
-            </form>
-        )
-    }
-}
-
-const mapStateToProps = state => ({
-    nome: state.nome,
-    email: state.email,
-    senha: state.senha
-})
-
-export default connect(mapStateToProps)(UsuarioForm)
+export default props => (
+    <form>
+        <div className="form-group">
+            <label>Nome</label>
+            <input className="form-control" id="nome" placeholder="Nome" value={props.nome}
+                onChange={props.onChangeNome} />
+        </div>
+        <div className="form-group">
+            <label>E-mail</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" 
+                value={props.email} onChange={props.onChangeEmail} placeholder="E-mail" />
+        </div>
+        <div className="form-group">
+            <label>Senha</label>
+            <input type="senha" className="form-control" id="senha" placeholder="Senha"
+                value={props.senha} onChange={props.onChangeSenha} />
+        </div>
+        <button onClick={props.onClickSalvar} className="btn btn-primary">Salvar</button>
+    </form>
+)
