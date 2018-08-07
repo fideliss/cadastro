d6784d31eb82a451a788a5453f2ddeb4f04cb696
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-08-07 09:05:22 -0300

    Removendo frontend para reconfiguração

--- src/main/frontend/webpack/main/app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import Usuario from '../usuario/usuario'
-
-export default props => (
-    <Usuario />
-)
--- src/main/frontend/webpack/usuario/usuarioForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default props => (
-    <div>
-        <h1>Form</h1>
-    </div>
-)
