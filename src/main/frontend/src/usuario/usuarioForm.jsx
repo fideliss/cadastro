7376412cd42e67d95d140d6263385742619a4985
Fidélis Elias da Silva <fidelis.silva@groupsoftware.com.br>
2018-08-08 08:37:33 -0300

    Melhorias no design da tela

--- src/main/frontend/src/usuario/usuarioForm.jsx
+++ src/main/frontend/src/usuario/usuarioForm.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
 export default props => (
-    <form>
+    <form className="form-horizontal">
         <div className="form-group">
-            <label>Nome</label>
-            <input className="form-control" id="nome" placeholder="Nome" value={props.nome}
-                onChange={props.onChangeNome} />
+            <label className="control-label col-sm-1">Nome</label>
+            <div className="col-sm-4">
+                <input className="form-control" id="nome" placeholder="Nome" value={props.nome}
+                    onChange={props.onChangeNome} />
+            </div>
         </div>
         <div className="form-group">
-            <label>E-mail</label>
-            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" 
-                value={props.email} onChange={props.onChangeEmail} placeholder="E-mail" />
+            <label className="control-label col-sm-1">E-mail</label>
+            <div className="col-sm-4">
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" 
+                    value={props.email} onChange={props.onChangeEmail} placeholder="E-mail" />
+            </div>
         </div>
         <div className="form-group">
-            <label>Senha</label>
-            <input type="senha" className="form-control" id="senha" placeholder="Senha"
-                value={props.senha} onChange={props.onChangeSenha} />
+            <label className="control-label col-sm-1">Senha</label>
+            <div className="col-sm-4">
+                <input type="senha" className="form-control" id="senha" placeholder="Senha"
+                    value={props.senha} onChange={props.onChangeSenha} />
+            </div>
         </div>
-        <button onClick={props.onClickSalvar} className="btn btn-primary">Salvar</button>
+        <div className="container col-sm-1">
+            <button onClick={props.onClickSalvar} className="btn btn-primary">Salvar</button>
+        </div>
     </form>
 )
